5ddf10ee7c8781fe739796fdefa8c4b20dfa9ba7
Suzu Tomo <47011206+kamekyame@users.noreply.github.com>
2021-08-29 14:58:13 +0900

    versionを1.0.0に変更

--- tools/make.ts
+++ tools/make.ts
@@ -10,7 +10,7 @@ const moduleData = new Domino.File({
   name: "Electone",
   folder: "YAMAHA",
   fileCreator: "SuzuTomo",
-  fileVersion: "0.1.0",
+  fileVersion: "1.0.0",
 }, { controlChangeMacroList: ccmList, templateList });
 
 const instrumentList = moduleData.createInstrumentList();
