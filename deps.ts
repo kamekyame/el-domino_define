a6c42052dd17dde8b55dcd62d545724ed6200921
Suzu Tomo <kamekyame@outlook.com>
2022-06-30 18:49:02 +0900

    ci(create-release): リリース自動化Github Actionsを追加

--- deps.ts
+++ deps.ts
@@ -1,4 +1,6 @@
 // Copyright 2022 kamekyame. All rights reserved. MIT license.
 
+export { expandGlob } from "https://deno.land/std@0.144.0/fs/mod.ts";
+
 // export * as Domino from "../donimo-define-deno/mod.ts";
 export * as Domino from "https://deno.land/x/domino_define@v0.4.0/mod.ts";
